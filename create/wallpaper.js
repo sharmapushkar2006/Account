32549d81e8f8682a4115f05c67e457dfc59ec6c2
Pushkar Sharma <258246915+sharmapushkar2006@users.noreply.github.com>
2026-07-22 14:03:07 +0530

    Fix image URL path in setWallpaper function

--- create/wallpaper.js
+++ create/wallpaper.js
@@ -3,7 +3,7 @@ const setWallpaper = () => {
   const arr = ['img1.jpg', 'img2.jpg', 'img3.jpg', 'img4.jpg', 'img5.jpg'];
   const randomIndex = Math.floor(Math.random() * arr.length);
   const selectedImage = arr[randomIndex];
-  const url = `https://sharmapushkar2006.github.io/Login/assets/${selectedImage}`;
+  const url = "../Assets/"+selectedImages;
 
   document.body.style.backgroundColor = '#f4f7ff';
   document.body.style.backgroundImage = `linear-gradient(135deg, rgba(27, 27, 29, 0.29), rgba(19, 20, 22, 0.33)), url('${url}')`;
