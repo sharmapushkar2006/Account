d822c64efc9c953d92012f7c9a9e1b9a23c55eee
Pushkar Sharma <258246915+sharmapushkar2006@users.noreply.github.com>
2026-08-07 23:45:58 +0530

    Implement account details validation
    Add validation for missing account details and alert user.

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 const username= localStorage.getItem("username");
 const email= localStorage.getItem("email");
 const token= localStorage.getItem("token");
+const changepassfield 
+if(!token || !username || !email) {
+  alert('Some details related to your account are missing');
+  location.href='Logout'
+}
 
+const changePassword =()=>{
+  
+}
